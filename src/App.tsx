1813cf6f1e7db1ec1b09fde9194b3750caf70de6
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2022-10-20 13:42:22 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  );
+}
+
+export default App;
